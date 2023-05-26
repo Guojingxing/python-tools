35e9cff77a1bcbab828df93966f01e7d34c168b0
Guo Jingxing <33157458+Guojingxing@users.noreply.github.com>
2023-05-26 11:26:21 +0800

    Add files via upload

---
+++ execute_py.bat
@@ -0,0 +1,46 @@
+@echo off
+chcp 65001
+setlocal enabledelayedexpansion
+
+REM 获取当前文件夹下的所有子文件夹中的.py文件
+set /a count=1
+for /d %%I in (*) do (
+    pushd "%%I"
+    for %%F in (*.py) do (
+        REM 读取.py文件中的doc描述
+        for /f "tokens=*" %%D in ('type "%%F" ^| findstr /i /c:"\"\"\"') do (
+            REM 去除doc描述中的引号和空格
+            set "desc=%%D"
+            set "desc=!desc:\"=!"
+            set "desc=!desc: =!"
+            REM 显示文件编号、描述和文件名
+            echo !count!. !desc! - %%F
+            set /a count+=1
+        )
+    )
+    popd
+)
+
+REM 提示用户选择要执行的py文件
+set /p choice=请输入要执行的文件编号：
+
+REM 执行用户选择的py文件
+set /a exec_count=1
+for /d %%I in (*) do (
+    pushd "%%I"
+    for %%F in (*.py) do (
+        if !exec_count! equ %choice% (
+            echo 执行 %%F
+            python "%%F"
+            goto :end
+        )
+        set /a exec_count+=1
+    )
+    popd
+)
+
+REM 用户选择的编号无效
+echo 无效的文件编号。
+
+:end
+endlocal
